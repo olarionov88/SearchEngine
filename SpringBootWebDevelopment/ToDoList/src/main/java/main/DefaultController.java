0b3d8f18d4508b794ff43e5a4f04670f1a8a0fb7
oleg_larionov <o_larionov_1988@mail.ru>
2023-05-10 22:28:34 +0500

    Homework 19.5 done

---
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Random;
+
+@RestController
+public class DefaultController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Random: " + new Random().nextInt(100000);
+    }
+}
