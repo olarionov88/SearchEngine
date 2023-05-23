4829428795a2f6eafe8e06910af14bf0bebfd5af
oleg_larionov <o_larionov_1988@mail.ru>
2023-05-23 15:52:39 +0500

    Homework 22.7 done

--- Performance/CarNumberGenerator/src/Loader.java
+++ Performance/CarNumberGenerator/src/Loader.java
@@ -1,41 +1,15 @@
-import java.io.FileOutputStream;
-
 public class Loader {
-
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
+        Writer thread_1 = new Writer(199, "res/numbers_1.txt");
+        Writer thread_2 = new Writer(199, "res/numbers_2.txt");
 
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        thread_1.start();
+        thread_2.start();
 
-        char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-        for (int number = 1; number < 1000; number++) {
-            int regionCode = 199;
-            for (char firstLetter : letters) {
-                for (char secondLetter : letters) {
-                    for (char thirdLetter : letters) {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
-                    }
-                }
-            }
-        }
-
-        writer.flush();
-        writer.close();
+        thread_1.join();
+        thread_2.join();
 
         System.out.println((System.currentTimeMillis() - start) + " ms");
-    }
-
-    private static String padNumber(int number, int numberLength) {
-        String numberStr = Integer.toString(number);
-        int padSize = numberLength - numberStr.length();
-
-        for (int i = 0; i < padSize; i++) {
-            numberStr = '0' + numberStr;
-        }
-
-        return numberStr;
     }
 }
